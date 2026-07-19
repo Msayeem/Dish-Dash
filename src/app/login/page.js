de6f46a169b5c51e4963c1e63580015d3b11e2df
Msayeem <msayeem223@gmail.com>
2026-07-19 22:54:48 +0600

    Implemented Google login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { signIn } from "@/lib/auth-client";
+import { authClient, signIn } from "@/lib/auth-client";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -44,6 +44,14 @@ export default function LoginPage() {
       setLoading(false);
     }
   };
+
+
+  const handleGoogle = async () => {
+  await authClient.signIn.social({
+    provider: "google",
+    callbackURL: "/",
+  });
+};
 
   return (
     <div className="flex flex-1 items-center justify-center px-4 py-12 sm:px-6 lg:px-8 bg-zinc-950">
@@ -127,6 +135,30 @@ export default function LoginPage() {
             Sign up for free
           </Link>
         </div>
+
+  <div className="relative my-8">
+          <div className="absolute inset-0 flex items-center">
+            <div className="w-full border-t border-white/[0.08]"></div>
+          </div>
+          <div className="relative flex justify-center text-xs uppercase tracking-wider">
+            <span className="px-4 bg-[#0a0b10]/40 backdrop-blur-sm text-slate-500 font-bold text-[10px]">Or continue with</span>
+          </div>
+        </div>
+
+   <button 
+          onClick={handleGoogle}
+          type="button"
+          className="w-full flex items-center justify-center gap-3 bg-white/[0.02] border border-white/[0.08] text-slate-200 hover:text-white hover:bg-white/[0.06] hover:border-white/[0.15] h-12 rounded-xl text-xs font-bold transition-all duration-200 shadow-sm uppercase tracking-wider cursor-pointer active:scale-[0.99]"
+        >
+          <svg viewBox="0 0 24 24" width="18" height="18" xmlns="http://www.w3.org/2000/svg" className="opacity-90 group-hover:opacity-100">
+            <path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/>
+            <path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/>
+            <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/>
+            <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
+          </svg>
+          Google
+        </button>
+
       </div>
     </div>
   );
